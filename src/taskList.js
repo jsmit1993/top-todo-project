aa2487c74ee90a8dfb955a750b6c610603658f01
jsmit1993 <156153441+jsmit1993@users.noreply.github.com>
2026-09-24 19:02:36 -0400

    build out task form functionality
    The task form works and adds tasks to the screen below the section.

--- src/taskList.js
+++ src/taskList.js
@@ -1,7 +1,43 @@
-import { addTask } from "./todoLogic";
+import { mainPageLoad } from "./initialLoad";
+import { addTask, projects, tasks } from "./todoLogic";
 
-function buildTaskDiv() {
+function buildTaskDiv(task) {
+    const individualTask = document.createElement('div');
+    individualTask.classList.add('individualTask');
+    
+    const individualTaskTitle = document.createElement('h3');
+    individualTaskTitle.classList.add('individualTaskTitle');
+    individualTaskTitle.textContent = task.title;
 
+    const individualDescription = document.createElement('p');
+    individualDescription.classList.add('individualDescipt');
+    individualDescription.textContent = task.description;
+
+    const individalDueDate = document.createElement('p');
+    individalDueDate.classList.add('individualDueDate');
+    individalDueDate.textContent = task.dueDate;
+
+    const individualTag = document.createElement('p');
+    individualTag.classList.add('individualTag');
+    individualTag.textContent = task.tag;
+
+    const individualProject = document.createElement('p');
+    individualProject.classList.add('individualProject');
+    individualProject.textContent = task.project;
+
+    const individualCompetion = document.createElement('p');
+    individualCompetion.classList.add('individualComp');
+    individualCompetion.textContent = task.completed;
+
+    
+    individualTask.appendChild(individualTaskTitle);
+    individualTask.appendChild(individualDescription);
+    individualTask.appendChild(individalDueDate);
+    individualTask.appendChild(individualTag);
+    individualTask.appendChild(individualProject);
+
+    individualTask.appendChild(individualCompetion);
+    return individualTask;
 }
 
 
@@ -26,22 +62,61 @@ function buildTaskForm() {
     const taskDescDiv = document.createElement('label');
     taskDescDiv.classList.add('addTaskdiv1');
     taskDescDiv.textContent = 'Description';
+    taskDescDiv.htmlFor = 'taskDescpField';
     const taskDescField = document.createElement('input');
+    taskDescField.id = 'taskDescpField';
     taskDescField.type = 'text';
 
     const taskDueDate = document.createElement('label');
     taskDueDate.classList.add('addTaskdiv1');
     taskDueDate.textContent = 'Due Date';
+    taskDueDate.htmlFor = 'taskDueDate';
     const taskDueDateField = document.createElement('input');
+    taskDueDateField.id = 'taskDueDate';
     taskDueDateField.type = 'date';
 
     const taskTagdiv = document.createElement('label');
     taskTagdiv.classList.add('addTaskdiv1');
     taskTagdiv.textContent = 'Tag';
+    taskTagdiv.htmlFor = 'taskTag';
     const taskTagField = document.createElement('input');
+    taskTagField.id = 'taskTag'
     taskTagField.type = 'text';
 
-    //need to see completed and project but then the id fields are not visible
+    const taskComplete = document.createElement('label');
+    taskComplete.classList.add('addTaskdiv1');
+    taskComplete.textContent = 'Completed?';
+    taskComplete.htmlFor = 'taskComplete';
+    const taskCompleteSelect = document.createElement('select');
+    taskCompleteSelect.id = 'taskComplete';
+    const optionY = document.createElement('option');
+    optionY.value = 'yes';
+    optionY.textContent = 'Yes';
+    const optionN = document.createElement('option');
+    optionN.value = 'no';
+    optionN.textContent = 'No';
+    taskCompleteSelect.appendChild(optionN);
+    taskCompleteSelect.appendChild(optionY);
+
+    const projectAssignment = document.createElement('label');
+    projectAssignment.classList.add('addTaskdiv1');
+    projectAssignment.textContent = 'Project';
+    projectAssignment.htmlFor = 'projectAssigning';
+    const projectSelect = document.createElement('select');
+    projectSelect.id = 'projectAssigning';
+
+    projects.forEach(project => {
+        const option = document.createElement('option');
+        option.value = project.projectID;
+        option.textContent = project.name;
+        projectSelect.appendChild(option);
+    })
+
+    const submitButton = document.createElement('button');
+    submitButton.type = 'submit';
+    submitButton.classList.add('taskSubmitButton');
+    submitButton.textContent = '+ Task';
+    
     addTaskForm.appendChild(taskName);
     addTaskForm.appendChild(taskNameField);
     addTaskForm.appendChild(taskDescDiv);
@@ -50,18 +125,54 @@ function buildTaskForm() {
     addTaskForm.appendChild(taskDueDateField);
     addTaskForm.appendChild(taskTagdiv);
     addTaskForm.appendChild(taskTagField);
+    addTaskForm.appendChild(taskComplete);
+    addTaskForm.appendChild(taskCompleteSelect);
+    addTaskForm.appendChild(projectAssignment);
+    addTaskForm.appendChild(projectSelect);
+    addTaskForm.appendChild(submitButton);
 
-    //title, description, dueDate, tag, completed, project, projectID, taskID
+    //event listener for all the stuff above on the button????
     addTaskdiv.appendChild(addTaskForm);
     
+    addTaskForm.addEventListener('submit', addTaskfromForm);
     return addTaskdiv;
+}
+
+function addTaskfromForm(e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const projectSelect = form.querySelector('#projectAssigning');
+    const selectProjectID = projectSelect.value;
+    const matchedProjectName = projects.find(p => p.projectID === selectProjectID);
+    const projectName = matchedProjectName ? matchedProjectName.name : 'Unassigned';
+
+    addTask(
+        form.querySelector('#taskNameInput').value,
+        form.querySelector('#taskDescpField').value,
+        form.querySelector('#taskDueDate').value,
+        form.querySelector('#taskTag').value,
+        form.querySelector('#taskComplete').value,
+        projectName,
+        selectProjectID,
+        crypto.randomUUID()
+    )
+    form.reset();
+    mainPageLoad();
 }
 
 function buildTasks() {
     const taskSectionTitle = document.createElement('div');
     taskSectionTitle.classList.add('taskListArea')
-    taskSectionTitle.textContent = 'Tasks';
+    const taskSectionHeader = document.createElement('h2');
+    taskSectionHeader.classList.add('taskHeader');
+    taskSectionHeader.textContent = 'Task List';
 
+    taskSectionTitle.appendChild(taskSectionHeader);
+
+    tasks.forEach(task => {
+        taskSectionTitle.appendChild(buildTaskDiv(task))
+    })
     return taskSectionTitle
 }
 
